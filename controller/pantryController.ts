ad53bde0b529c30252edc90b66d0b0e06c7ad784
Jacob <jacobkrodgers@gmail.com>
2025-04-13 18:10:47 -0500

    Add Pantry and Shopping Cart Schema

---
+++ controller/pantryController.ts
@@ -0,0 +1,95 @@
+import { get_user_by_api_key } from "@/model/userModel";
+import {
+  get_pantry_by_user_id,
+  get_shopping_cart_by_user_id,
+  add_pantry_ingredient_by_user_id,
+  add_shopping_cart_ingredient_by_user_id,
+  delete_pantry_ingredient_by_id,
+  delete_shopping_cart_ingredient_by_id,
+  update_pantry_ingredient_by_id,
+  update_shopping_cart_ingredient_by_id,
+} from "@/model/pantryModel";
+import { ServerUser } from "@/type/User";
+import { Ingredient } from "@/type/Recipe";
+import { Pantry } from "@/type/Pantry";
+import { GenericAPIResponse, ActionResponse } from "@/type/Generic";
+
+export async function getPantryByApiKey(apiKey: string): Promise<ActionResponse<Pantry>> {
+  const user: ServerUser | null = await get_user_by_api_key(apiKey);
+  if (!user) return { message: "Unauthorized", status: 401 };
+
+  const pantry = await get_pantry_by_user_id(user.id);
+  if (!pantry) return { message: "Pantry not found", status: 404 };
+
+  return { payload: pantry, status: 200 };
+}
+
+export async function getShoppingCartByApiKey(apiKey: string): Promise<ActionResponse<Pantry>> {
+  const user: ServerUser | null = await get_user_by_api_key(apiKey);
+  if (!user) return { message: "Unauthorized", status: 401 };
+
+  const cart = await get_shopping_cart_by_user_id(user.id);
+  if (!cart) return { message: "Shopping cart not found", status: 404 };
+
+  return { payload: cart, status: 200 };
+}
+
+export async function addPantryIngredientByApiKey(apiKey: string, ingredient: Ingredient): Promise<ActionResponse<Ingredient>> {
+  const user: ServerUser | null = await get_user_by_api_key(apiKey);
+  if (!user) return { message: "Unauthorized", status: 401 };
+
+  const added = await add_pantry_ingredient_by_user_id(user.id, ingredient);
+  if (!added) return { message: "Unable to add ingredient", status: 500 };
+
+  return { payload: added, status: 200 };
+}
+
+export async function addShoppingCartIngredientByApiKey(apiKey: string, ingredient: Ingredient): Promise<ActionResponse<Ingredient>> {
+  const user: ServerUser | null = await get_user_by_api_key(apiKey);
+  if (!user) return { message: "Unauthorized", status: 401 };
+
+  const added = await add_shopping_cart_ingredient_by_user_id(user.id, ingredient);
+  if (!added) return { message: "Unable to add ingredient", status: 500 };
+
+  return { payload: added, status: 200 };
+}
+
+export async function deletePantryIngredientById(apiKey: string, ingredientId: string): Promise<ActionResponse<string>> {
+  const user: ServerUser | null = await get_user_by_api_key(apiKey);
+  if (!user) return { message: "Unauthorized", status: 401 };
+
+  const deleted = await delete_pantry_ingredient_by_id(ingredientId);
+  if (!deleted) return { message: "Ingredient not found", status: 404 };
+
+  return { payload: "Ingredient deleted", status: 200 };
+}
+
+export async function deleteShoppingCartIngredientById(apiKey: string, ingredientId: string): Promise<ActionResponse<string>> {
+  const user: ServerUser | null = await get_user_by_api_key(apiKey);
+  if (!user) return { message: "Unauthorized", status: 401 };
+
+  const deleted = await delete_shopping_cart_ingredient_by_id(ingredientId);
+  if (!deleted) return { message: "Ingredient not found", status: 404 };
+
+  return { payload: "Ingredient deleted", status: 200 };
+}
+
+export async function updatePantryIngredientById(apiKey: string, ingredientId: string, updatedData: Partial<Ingredient>): Promise<ActionResponse<Ingredient>> {
+  const user: ServerUser | null = await get_user_by_api_key(apiKey);
+  if (!user) return { message: "Unauthorized", status: 401 };
+
+  const updated = await update_pantry_ingredient_by_id(ingredientId, updatedData);
+  if (!updated) return { message: "Unable to update ingredient", status: 404 };
+
+  return { payload: updated, status: 200 };
+}
+
+export async function updateShoppingCartIngredientById(apiKey: string, ingredientId: string, updatedData: Partial<Ingredient>): Promise<ActionResponse<Ingredient>> {
+  const user: ServerUser | null = await get_user_by_api_key(apiKey);
+  if (!user) return { message: "Unauthorized", status: 401 };
+
+  const updated = await update_shopping_cart_ingredient_by_id(ingredientId, updatedData);
+  if (!updated) return { message: "Unable to update ingredient", status: 404 };
+
+  return { payload: updated, status: 200 };
+}
---
+++ model/pantryModel.ts
@@ -0,0 +1,137 @@
+import { prisma } from "@/prisma/dbClient";
+import { Pantry } from "@/type/Pantry";
+import { Ingredient } from "@/type/Recipe";
+
+export async function get_pantry_by_user_id(userId: string): 
+    Promise<Pantry | null>
+{
+    const pantry = await prisma.pantry.findUnique({
+        where: { userId },
+        include: {
+            ingredients: true,
+        },
+    });
+  
+    if (!pantry) {
+        return null;
+    }
+  
+    return pantry;
+}
+
+export async function get_shopping_cart_by_user_id(userId: string): 
+    Promise<Pantry | null>
+{
+    const shoppingCart = await prisma.shoppingCart.findUnique({
+        where: { userId },
+        include: {
+            ingredients: true,
+        },
+    });
+  
+    if (!shoppingCart) {
+        return null;
+    }
+  
+    return shoppingCart;
+}
+
+export async function add_pantry_ingredient_by_user_id(userId: string, ingredient: Ingredient):
+    Promise<Ingredient | null>
+{
+    const pantry = await prisma.pantry.findUnique({
+        where: { userId },
+    });
+  
+    if (!pantry) {
+        return null;
+    }
+  
+    const pantryIngredient = await prisma.ingredient.create({
+        data: {
+            name: ingredient.name,
+            quantity: ingredient.quantity,
+            quantityUnit: ingredient.quantityUnit,
+            form: ingredient.form,
+            pantryId: pantry.id,
+        },
+    });
+  
+    return pantryIngredient;
+}
+
+export async function add_shopping_cart_ingredient_by_user_id(userId: string, ingredient: Ingredient):
+    Promise<Ingredient | null>
+{
+    const shoppingCart = await prisma.shoppingCart.findUnique({
+        where: { userId },
+    });
+  
+    if (!shoppingCart) {
+        return null;
+    }
+  
+    const shoppingCartIngredient = await prisma.ingredient.create({
+        data: {
+            name: ingredient.name,
+            quantity: ingredient.quantity,
+            quantityUnit: ingredient.quantityUnit,
+            form: ingredient.form,
+            pantryId: shoppingCart.id,
+        },
+    });
+  
+    return shoppingCartIngredient;
+}
+
+export async function delete_pantry_ingredient_by_id(ingredientId: string):
+    Promise<Ingredient | null>
+{
+    const deletedIngredient = await prisma.ingredient.delete({
+        where: { id: ingredientId },
+    });
+  
+    return deletedIngredient;
+}
+
+export async function delete_shopping_cart_ingredient_by_id(ingredientId: string) 
+{
+    const deletedIngredient = await prisma.shoppingCart.delete({
+        where: { id: ingredientId },
+    });
+  
+    return deletedIngredient;
+}
+
+export async function update_pantry_ingredient_by_id(ingredientId: string, updatedData: Partial<Ingredient>):
+    Promise<Ingredient | null>
+{
+    const updatedIngredient = await prisma.ingredient.update({
+        where: { id: ingredientId },
+        data: {
+            name: updatedData.name,
+            quantity: updatedData.quantity,
+            quantityUnit: updatedData.quantityUnit,
+            form: updatedData.form,
+        },
+    });
+  
+    return updatedIngredient;
+}
+
+export async function update_shopping_cart_ingredient_by_id(ingredientId: string, updatedData: Partial<Ingredient>):
+    Promise<Ingredient | null>
+{
+    const updatedIngredient = await prisma.ingredient.update({
+        where: { id: ingredientId },
+        data: {
+            name: updatedData.name,
+            quantity: updatedData.quantity,
+            quantityUnit: updatedData.quantityUnit,
+            form: updatedData.form,
+        },
+    });
+  
+    return updatedIngredient;
+}
+  
