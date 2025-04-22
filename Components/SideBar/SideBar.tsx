d803bcda7cc57f4990fc2b27688ebc27e9687c68
Jacob <jacobkrodgers@gmail.com>
2025-04-22 10:05:06 -0500

    Set enableColorOnDark and set color to white

--- Components/SideBar/SideBar.tsx
+++ Components/SideBar/SideBar.tsx
@@ -156,7 +156,15 @@ export default function SideBar({ user, children }: SideBarProps) {
 
             </Drawer>
             <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
-                <AppBar position="sticky" elevation={1} sx={{ backgroundColor: theme.palette.primary.main }}>
+                <AppBar 
+                    position="sticky" 
+                    elevation={1} 
+                    sx={{ 
+                        backgroundColor: theme.palette.primary.main,
+                        color: "white"
+                        }} 
+                    enableColorOnDark
+                >
                     <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', px: 2 }}>
                         <Link
                             href="/"
